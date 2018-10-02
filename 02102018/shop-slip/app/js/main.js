6feeadc92f7926972575b1513748625800179e22
frankekko91 <frankekko91@gmail.com>
2018-10-02 23:29:38 +0200

    Update shop slip

--- 02102018/shop-slip/app/js/main.js
+++ 02102018/shop-slip/app/js/main.js
@@ -31,12 +31,39 @@ document.addEventListener("DOMContentLoaded", function () {
     // Add event to listener
     document.addEventListener("fetchEnd", (event) => {
         var data = event.detail;
-        console.log(data);
+        // console.log(data);
          for (let i = 0; i < data.productList.length ; i++) {
           const element  = data.productList[i];
-          console.log(element);
+        //   console.log(element);
         }
- 
+    var sizeCheck = document.querySelectorAll('input[name=checkbox-size]');
+    const divyArray = Array.from(sizeCheck);
+    console.log(divyArray);
+    divyArray.forEach( el => {
+        el.addEventListener('change', function() {
+        if(el.value === "S") {
+            console.log('ciao');
+            for(let i = 0; i < data.productList.length; i++ ) {
+            if(data.productList[i].size === "S") {
+                var price = data.productList[i].price;
+                document.getElementById('price').innerText = "Prezzo: " + price + data.currency;             
+            }
+           }   
+        } else if (el.value === "M") {
+            for(let i = 0; i < data.productList.length; i++ ) {
+                if(data.productList[i].size === "M") {
+                    var price = data.productList[i].price;
+                    document.getElementById('price').innerText = "Prezzo: " + price + data.currency;
+                    if(data.productList[i].color != "red") {
+                        document.getElementById('red').setAttribute('disabled',false);
+
+
+                    } 
+                }
+            } 
+        }
+    })
+})
         
     })
     // call the initializer function
