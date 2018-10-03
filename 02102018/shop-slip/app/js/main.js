874f53e5f21b4707957dad9557fb1721749ea238
frankekko91 <frankekko91@gmail.com>
2018-10-03 17:58:14 +0200

    Update slip-shop

--- 02102018/shop-slip/app/js/main.js
+++ 02102018/shop-slip/app/js/main.js
@@ -16,9 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
                     response.json().then(function (data) {
                         // Create event 
-                        var event = new CustomEvent('fetchEnd', { "detail": data });
+                        var event = new CustomEvent('fetchEnd', {
+                            "detail": data
+                        });
                         //console.log("dispatch fetchEnd");
-                        document.dispatchEvent(event);   // dispatching event
+                        document.dispatchEvent(event); // dispatching event
                     });
                 }
             )
@@ -32,41 +34,93 @@ document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("fetchEnd", (event) => {
         var data = event.detail;
         // console.log(data);
-         for (let i = 0; i < data.productList.length ; i++) {
-          const element  = data.productList[i];
-        //   console.log(element);
+        for (let i = 0; i < data.productList.length; i++) {
+            const element = data.productList[i];
+            //   console.log(element);
         }
-    var sizeCheck = document.querySelectorAll('input[name=checkbox-size]');
-    const divyArray = Array.from(sizeCheck);
-    console.log(divyArray);
-    divyArray.forEach( el => {
-        el.addEventListener('change', function() {
-        if(el.value === "S") {
-            console.log('ciao');
-            for(let i = 0; i < data.productList.length; i++ ) {
-            if(data.productList[i].size === "S") {
-                var price = data.productList[i].price;
-                document.getElementById('price').innerText = "Prezzo: " + price + data.currency;             
-            }
-           }   
-        } else if (el.value === "M") {
-            for(let i = 0; i < data.productList.length; i++ ) {
-                if(data.productList[i].size === "M") {
-                    var price = data.productList[i].price;
-                    document.getElementById('price').innerText = "Prezzo: " + price + data.currency;
-                    if(data.productList[i].color != "red") {
-                        document.getElementById('red').setAttribute('disabled',false);
+        var sizeCheck = document.querySelectorAll('input[name=checkbox-size]');
+        var image = document.getElementById('product-image');
+        var red = document.getElementById('red');
+        var black = document.getElementById('black');
+        const divyArray = Array.from(sizeCheck);
+        console.log(divyArray);
+        divyArray.forEach(el => {
+            el.addEventListener('change', function () {
+                switch (el.value) {
+                    case "S":
+                        {
+                            for (let i = 0; i < data.productList.length; i++) {
+                                if (data.productList[i].size === "S") {
+                                    var price = data.productList[i].price;
+                                    document.getElementById('price').innerText = "Prezzo: " + price + data.currency;
+                                    if ((data.productList[i].color = "red") && (data.productList[i].color = "black")) {
+                                        image.innerHTML += '<img alt="" class="cell small-6" src="img/slip-red.jpg"/>';
+                                        red.removeAttribute("disabled");  // 
+                                        black.removeAttribute("disabled"); //
+                                    }
+                                    // if (data.productList[i].color != "black") {
+                                    //     black.setAttribute('disabled', false);
+                                    // } else {
+                                    // }
+                                }
+                            }
+                        }
+                        break;
+                    case "M":
+                        {
+                            for (let i = 0; i < data.productList.length; i++) {
+                                if (data.productList[i].size === "M") {
+                                    var price = data.productList[i].price;
+                                    document.getElementById('price').innerText = "Prezzo: " + price + data.currency;
+                                    if (data.productList[i].color != "red") {
+                                        document.getElementById('red').setAttribute('disabled', false);
+                                    } else if (data.productList[i].color != "black") {
+                                        document.getElementById('black').setAttribute('disabled', false);
+                                    } else {
 
+                                    }
+                                }
+                            }
+                        }
+                        break;
 
-                    } 
                 }
-            } 
-        }
-    })
-})
-        
+                /* if (el.value === "S") {
+                    console.log('ciao');
+                    for (let i = 0; i < data.productList.length; i++) {
+                        if (data.productList[i].size === "S") {
+                            var price = data.productList[i].price;
+                            document.getElementById('price').innerText = "Prezzo: " + price + data.currency;
+                            if (data.productList[i].color = "red") {
+                                image.innerHTML += '<img alt="" class="cell small-6" src="img/slip-red.jpg"/>';
+                                // document.getElementById('red').setAttribute('disabled', false);
+                            }
+                            if (data.productList[i].color != "black") {
+                                document.getElementById('black').setAttribute('disabled', false);
+                            } else {
+                                //image.innerHTML+='<img alt="" class="cell small-6" src="img/slip-red.jpg"/>';
+                            }
+                        }
+                    }
+                } else if (el.value === "M") {
+                    for (let i = 0; i < data.productList.length; i++) {
+                        if (data.productList[i].size === "M") {
+                            var price = data.productList[i].price;
+                            document.getElementById('price').innerText = "Prezzo: " + price + data.currency;
+                            if (data.productList[i].color != "red") {
+                                document.getElementById('red').setAttribute('disabled', false);
+                            } else if (data.productList[i].color != "black") {
+                                document.getElementById('black').setAttribute('disabled', false);
+                            } else {
+
+                            }
+                        }
+                    }
+                } */
+            })
+        })
     })
     // call the initializer function
     takeData();
 
-    });
+});
